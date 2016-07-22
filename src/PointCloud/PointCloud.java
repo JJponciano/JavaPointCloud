263c167669502662f59e46f82cb75cfb661f25c8
Jean-Jacques PONCIANO <ponciano.jeanjacques@gmail.com>
2016-07-22 13:31:47 +0200

    clean algorithms contained in the PointCloud class and remove its interface

--- src/PointCloud/PointCloud.java
+++ src/PointCloud/PointCloud.java
@@ -19,29 +19,13 @@
 package PointCloud;
 
 import java.awt.Color;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author Jean-Jacques Ponciano.
  */
-public class PointCloud implements IPointCloud {
+public class PointCloud {
 
     protected ArrayList<PointColor> points;
     protected final String ext;
@@ -54,163 +38,10 @@ public class PointCloud implements IPointCloud {
         this.points = new ArrayList<>();
     }
 
-    @Override
-    public void loadTXT(String filepath) throws FileNotFoundException {
-        //read the file
-        File fileio = new File(filepath);
-        try (BufferedReader reader = Files.newBufferedReader(fileio.toPath(),
-                StandardCharsets.UTF_8)) {
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                //test if the line is not a comment
-                if (line.charAt(0) != '#') {
-                    //if it not a comment, split the line in different value
-                    String[] split = line.split("\\s");
-                    PointColor p = new PointColor(Float.parseFloat(split[0]), Float.parseFloat(split[1]), Float.parseFloat(split[2]));
-                    if (split.length == 6) {
-                        p.setColor(new Color(Integer.parseInt(split[3]), Integer.parseInt(split[4]), Integer.parseInt(split[5])));
-                    }
-                    this.points.add(p);
-                }
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(PointCloud.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    @Override
-    public void loadBin(String filepath) throws FileNotFoundException {
-        //create the file
-        File file = new File(filepath);
-        // test if the file exists
-        if (!file.exists()) // if it does not exists throws a exception.
-        {
-            throw new java.io.FileNotFoundException("the file with the path " + filepath + "does not found");
-        }
-        DataInputStream ois = null;
-        try {
-            ois = new DataInputStream(new BufferedInputStream(new FileInputStream(filepath)));
-            //read number of point
-            int nbPoint = ois.readInt();
-            this.points.clear();
-            //read and build each point
-            for (int i = 0; i < nbPoint; i++) {
-                float x = ois.readFloat();
-                float y = ois.readFloat();
-                float z = ois.readFloat();
-                this.points.add(new PointColor(x, y, z));
-            }
-            ois.close();
-        } catch (final java.io.IOException e) {
-            System.err.println(e);
-        } finally {
-            try {
-                if (ois != null) {
-                    ois.close();
-                }
-            } catch (final IOException ex) {
-                System.err.println(ex);
-            }
-        }
-    }
-
-    @Override
-    public void saveBin(String filepath) {
-        // initialization of the stream.
-        DataOutputStream oos = null;
-        try {
-            // open the stream from the file
-            oos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(filepath)));
-
-            //write the number of point
-            oos.writeInt(points.size());
-            for (Point point : points) {
-                // write points coordinates
-                oos.writeFloat(point.getX());
-                oos.writeFloat(point.getY());
-                oos.writeFloat(point.getZ());
-            }
-
-        } catch (final java.io.IOException e) {
-            System.err.println(e);
-        } finally {
-            try {
-                if (oos != null) {
-                    // empty the buffer
-                    oos.flush();
-                    // close the file
-                    oos.close();
-                }
-            } catch (final IOException ex) {
-                System.err.println(ex);
-            }
-        }
-    }
-
-    @Override
-    public void saveTXT(String filepath, boolean color) throws FileNotFoundException {
-        String txt = this.toString();
-        File fileio = new File(filepath);
-        Charset charset = Charset.forName("UTF8");
-        try (BufferedWriter writer = Files.newBufferedWriter(fileio.toPath(), charset)) {
-            writer.write(txt, 0, txt.length());
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-    }
-
-    @Override
-    public void center() {
-        if (this.points.size() > 0) {
-            float[] orig = this.points.get(0).getArray();
-            for (int i = 1; i < this.points.size(); i++) {
-                this.points.get(i).setX(this.points.get(i).getX() - orig[0]);
-                this.points.get(i).setY(this.points.get(i).getY() - orig[1]);
-                this.points.get(i).setZ(this.points.get(i).getZ() - orig[2]);
-
-            }
-        }
-    }
-
-    @Override
-    public void scale(float x, float y, float z) {
-        for (int i = 0; i < this.points.size(); i++) {
-            this.points.get(i).setX(this.points.get(i).getX() * x);
-            this.points.get(i).setY(this.points.get(i).getY() * y);
-            this.points.get(i).setZ(this.points.get(i).getZ() * z);
-        }
-    }
-
-    @Override
-    public void switchYZ() {
-        for (int i = 0; i < this.points.size(); i++) {
-            float y = this.points.get(i).getY();
-            this.points.get(i).setY(this.points.get(i).getZ());
-            this.points.get(i).setZ(y);
-
-        }
-    }
-
-    @Override
-    public void switchZX() {
-        for (int i = 0; i < this.points.size(); i++) {
-            float z = this.points.get(i).getZ();
-            this.points.get(i).setZ(this.points.get(i).getX());
-            this.points.get(i).setX(z);
-
-        }
-    }
-
-    @Override
     public void setColor(Color color) {
         for (int i = 0; i < this.points.size(); i++) {
             this.points.get(i).setColor(color);
         }
-    }
-
-    @Override
-    public String getEXT() {
-        return this.ext;
     }
 
     @Override
