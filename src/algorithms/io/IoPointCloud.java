5a392f976aa15917ec14b135b6a57dd6fbf566e5
Jean-Jacques PONCIANO <ponciano.jeanjacques@gmail.com>
2016-07-22 12:57:27 +0200

    Add new algorithm package **io**

--- src/algorithms/io/IoPointCloud.java
+++ src/algorithms/io/IoPointCloud.java
@@ -16,7 +16,9 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301  USA
  */
-package algorithms;
+package algorithms.io;
+
+import algorithms.IAlgorithm;
 
 /**
  *
--- src/algorithms/io/ReadPCfromTXT.java
+++ src/algorithms/io/ReadPCfromTXT.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301  USA
  */
-package algorithms;
+package algorithms.io;
 
 import PointCloud.PointCloud;
 import PointCloud.PointColor;
