a2984ff53a556d3f2a5a3c5d5418b507ac5e9c05
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-08-07 09:52:45 +0200

    Create SegmentationNormal.java

---
+++ src/main/java/info/ponciano/lab/jpc/algorithms/segmentation/SegmentationNormal.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2020 Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.ponciano.lab.jpc.algorithms.segmentation;
+
+import info.ponciano.lab.jpc.pointcloud.components.APointCloud;
+import info.ponciano.lab.jpc.pointcloud.stucture.octree.Voxel;
+
+public class SegmentationNormal extends Segmentation {
+
+    public SegmentationNormal(APointCloud region, double distance) {
+        super(region, distance);
+    }
+
+    public SegmentationNormal(Voxel next) {
+        super(next);
+    }
+
+    @Override
+    protected Region createsRegion(APointCloud points, double distance) {
+        return new RegionNormal(points, distance);
+    }
+}
