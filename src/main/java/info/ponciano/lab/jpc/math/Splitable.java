f56b31eb06a927d5dadd165f725d06483e60f107
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-08-07 09:53:10 +0200

    Create Splitable.java

---
+++ src/main/java/info/ponciano/lab/jpc/math/Splitable.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2020 Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.ponciano.lab.jpc.math;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
+ */
+public interface Splitable {
+     /**
+     * Split the box according to the dimension (4 box in 2D, 8 box in 3D).
+     *
+     * @return Sub box created after the split.
+     */
+      public ArrayList split() ;
+}
