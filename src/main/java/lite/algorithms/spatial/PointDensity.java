5ba86ee4dbb283f2059e994d1181a8e615eefe53
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-08-09 11:50:52 +0200

    Create PointDensity.java

---
+++ src/main/java/lite/algorithms/spatial/PointDensity.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2016 Jean-Jacques Ponciano.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package lite.algorithms.spatial;
+
+import lite.algorithms.IAlgorithm;
+import lite.pointcloud.PointCloud;
+
+/**
+ *
+ * @author Jean-Jacques Ponciano
+ */
+public class PointDensity implements IAlgorithm {
+
+    protected PointCloud cloud;
+    protected boolean isReady;
+    protected double density;
+    protected Unit unit;
+
+    /**
+     *  * Creates a new instance of <code>PointDensity</code>.
+     *
+     * @param cloud cloud to be used.
+     * @param unit Unit of spatial measure
+     */
+    public PointDensity(PointCloud cloud, Unit unit) {
+        this.cloud = cloud;
+        this.isReady = true;
+        this.density = 0;
+        this.unit = unit;
+    }
+
+    @Override
+    public boolean isReady() {
+        return this.isReady;
+    }
+
+    public PointCloud getCloud() {
+        return cloud;
+    }
+
+    @Override
+    public void run() {
+        this.isReady = false;
+        //Density calculating
+        //calculate the area
+        AreaPC areaPC = new AreaPC(cloud);
+        areaPC.run();
+        double area = areaPC.getArea(this.unit);
+        //divide the area by the number of points
+        this.density = (double) (area / (double) this.cloud.size());;
+        this.isReady = true;
+
+    }
+
+    public double getDensity() {
+        return density;
+    }
+
+}
