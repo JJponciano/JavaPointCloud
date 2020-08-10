5487a51abe8421a28ee598d7489f3ffcff9c170d
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-08-10 09:39:15 +0200

    Create MinPatchesDistanceEstimationTest.java

---
+++ src/test/java/info/ponciano/lab/jpc/algorithms/MinPatchesDistanceEstimationTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2020 Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.ponciano.lab.jpc.algorithms;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
+ */
+public class MinPatchesDistanceEstimationTest {
+    
+    public MinPatchesDistanceEstimationTest() {
+    }
+  
+
+    /**
+     * Test of run method, of class MinPatchesDistanceEstimation.
+     */
+    @Test
+    public void testRun() {
+        System.out.println("run");
+        MinPatchesDistanceEstimation instance = null;
+        instance.run();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getResults method, of class MinPatchesDistanceEstimation.
+     */
+    @Test
+    public void testGetResults() {
+        System.out.println("getResults");
+        MinPatchesDistanceEstimation instance = null;
+        Double expResult = null;
+        Double result = instance.getResults();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
