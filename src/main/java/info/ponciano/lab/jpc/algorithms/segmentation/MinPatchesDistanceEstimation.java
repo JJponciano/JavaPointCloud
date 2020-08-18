332c7c03444a92e1cb70481136094fad19d9c9a2
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-08-18 15:19:58 +0200

    Update MinPatchesDistanceEstimation.java

--- src/main/java/info/ponciano/lab/jpc/algorithms/segmentation/MinPatchesDistanceEstimation.java
+++ src/main/java/info/ponciano/lab/jpc/algorithms/segmentation/MinPatchesDistanceEstimation.java
@@ -14,8 +14,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package info.ponciano.lab.jpc.algorithms;
+package info.ponciano.lab.jpc.algorithms.segmentation;
 
+import info.ponciano.lab.jpc.algorithms.Algorithm;
 import info.ponciano.lab.jpc.pointcloud.components.APointCloud;
 
 /**
