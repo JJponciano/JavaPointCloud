89b58364932375f7d3990db58e8bee11d10bbe21
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-08-25 19:52:48 +0200

    Update Voxels.java

--- src/main/java/info/ponciano/lab/jpc/pointcloud/stucture/octree/Voxels.java
+++ src/main/java/info/ponciano/lab/jpc/pointcloud/stucture/octree/Voxels.java
@@ -24,7 +24,6 @@ import info.ponciano.lab.jpc.opengl.Display;
 import info.ponciano.lab.jpc.opengl.IObjectGL;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
