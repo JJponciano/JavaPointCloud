b8fdc20e11b295006feea8ed15880dd90da9de1e
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-08-26 10:20:52 +0200

    Create Segment.java

---
+++ src/main/java/info/ponciano/lab/jpc/pointcloud/Segment.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2020 Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.ponciano.lab.jpc.pointcloud;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * Class composed of Patches key reference.
+ *
+ * @author Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
+ */
+public class Segment {
+    
+    protected Set<String> patches;
+
+    public Segment() {
+        this.patches = new LinkedHashSet<>();
+    }
+
+    public void add(String patch) {
+        this.patches.add(patch);
+    }
+
+    public boolean contains(String patch) {
+        return this.contains(patch);
+    }
+}
