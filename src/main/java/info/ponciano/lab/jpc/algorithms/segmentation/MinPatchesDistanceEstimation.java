d7fdb03740970b25a12b463350ca596d8c987bb8
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-10-09 09:52:42 +0200

    Update MinPatchesDistanceEstimation.java

--- src/main/java/info/ponciano/lab/jpc/algorithms/segmentation/MinPatchesDistanceEstimation.java
+++ src/main/java/info/ponciano/lab/jpc/algorithms/segmentation/MinPatchesDistanceEstimation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
+ * Copyright (C) 2020 Dr Jean-Jacques Ponciano (Contact: jean-jacques@ponciano.info)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@ import info.ponciano.lab.jpc.pointcloud.components.APointCloud;
  * {@code MinPatchesDistanceEstimation } aims to calculate the minimum distance
  * between two point clouds according to their oriented bounding box.
  *
- * @author Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
+ * @author Dr Jean-Jacques Ponciano (Contact: jean-jacques@ponciano.info)
  */
 public class MinPatchesDistanceEstimation implements Algorithm<Double> {
 
