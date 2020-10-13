204f2c3333a74b9a9d7417002aedccfbb7b4f364
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-10-13 10:34:54 +0200

    Update PointCloudExample.java

--- src/main/java/info/ponciano/lab/jpc/examples/PointCloudExample.java
+++ src/main/java/info/ponciano/lab/jpc/examples/PointCloudExample.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
+ * Copyright (C) 2020 Dr Jean-Jacques Ponciano (Contact: jean-jacques@ponciano.info)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,7 +25,7 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
+ * @author Dr Jean-Jacques Ponciano (Contact: jean-jacques@ponciano.info)
  */
 public class PointCloudExample {
 
